6357244789cd364948a7966386a977f4b71c0939
Dmitry <ya.potapov@yandex.ru>
2022-10-12 13:17:47 +0300

    task6.py

---
+++ task6.py
@@ -0,0 +1,13 @@
+# Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+number = int(input("Введите число: "))
+lst = []
+for i in range(number):
+    lst.append((-3)**i)
+print(f"Итоговая последовательность: {lst}")
+
+# улучшение
+
+lst = [(-3)**i for i in range(number)]
+print(f"Итоговая последовательность после применения list comprehension: {lst}")
